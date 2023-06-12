438f9bbf7ca74b4f2dcc0d239f7b8d667bcfa335
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-12 17:44:54 +0300

    feat: add a function for updating the field to additional file

--- additional.py
+++ additional.py
@@ -11,3 +11,29 @@ def get_neighbours(pole, row, col):
         if 0 <= offset_row < rows and 0 <= offset_col < cols and pole[offset_row][offset_col] == 1:
             count += 1
     return count
+
+
+def update_pole(pole):
+    global cycle
+    new_pole = []
+    for row in range(len(pole)):
+        new_row = []
+        for col in range(len(pole[row])):
+            cell = pole[row][col]
+
+            live_neighbours = get_neighbours(pole, row, col)
+
+            if cell == 1:
+                if live_neighbours in [2, 3]:
+                    new_row.append(1)
+                else:
+                    cycle += 1
+                    new_row.append(0)
+            else:
+                if live_neighbours == 3:
+                    new_row.append(1)
+                else:
+                    new_row.append(0)
+
+        new_pole.append(new_row)
+    return new_pole
